eb1ed154ee836f32509d72bc25f75aa431b8ce09
shaakilahammed <shaakilahammed@gmail.com>
2022-04-06 15:48:39 +0600

    Facebook login using next-auth

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,10 @@
+import { getSession } from 'next-auth/client';
 import Head from 'next/head';
 import Header from '../components/Header';
+import Login from '../components/Login';
 
-export default function Home() {
+export default function Home({ session }) {
+  if (!session) return <Login />;
   return (
     <div>
       <Head>
@@ -13,4 +16,14 @@ export default function Home() {
       <main></main>
     </div>
   );
+}
+
+export async function getServerSideProps(context) {
+  const session = await getSession(context);
+
+  return {
+    props: {
+      session,
+    },
+  };
 }
